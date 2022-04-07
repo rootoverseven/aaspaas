24d5dd2dc64185dd837d427d374f819dfc7af778
Piyush Kumar <piyush.kumar@agnity.com>
2022-04-07 10:58:40 +0530

    first commit

---
+++ src/pages/other/LoginRegister.js
@@ -0,0 +1,154 @@
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import { Link } from "react-router-dom";
+import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
+import Tab from "react-bootstrap/Tab";
+import Nav from "react-bootstrap/Nav";
+import LayoutOne from "../../layouts/LayoutOne";
+import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
+import {signUpUser} from "../../api.js";
+import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { ToastProvider, useToasts } from 'react-toast-notifications';
+
+
+
+const LoginRegister = ({ location }) => {
+  const { addToast } = useToasts();
+
+  const history = useHistory();
+
+  const { pathname } = location;
+  let [userDetails, setUserDetails] = useState({
+    username: "",
+    password: "",
+    email: "",
+})
+async function submit(e) {
+    e.preventDefault()
+    try {
+        let res = await signUpUser(userDetails)
+        addToast('Registered Successfully', { appearance: 'success' });
+
+        // history.push('/sign-in')
+    } catch (error) {
+      addToast('Some error occured.', { appearance: 'error' });
+        console.log(error)
+    }
+}
+function changeHandle(e){
+    console.log(e.target.value)
+    setUserDetails(values => ({...values, [e.target.name]: e.target.value}))
+    // userDetails[e.target.name]=e.target.value;
+}
+  return (
+    <Fragment>
+      <MetaTags>
+        <title>Flone | Login</title>
+        <meta
+          name="description"
+          content="Compare page of flone react minimalist eCommerce template."
+        />
+      </MetaTags>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
+        Login Register
+      </BreadcrumbsItem>
+      <LayoutOne headerTop="visible">
+        {/* breadcrumb */}
+        <Breadcrumb />
+        <div className="login-register-area pt-100 pb-100">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div className="login-register-wrapper">
+                  <Tab.Container defaultActiveKey="login">
+                    <Nav variant="pills" className="login-register-tab-list">
+                      <Nav.Item>
+                        <Nav.Link eventKey="login">
+                          <h4>Login</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="register">
+                          <h4>Register</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="login">
+                        <div className="login-form-container">
+                          <div className="login-register-form">
+                            <form>
+                              <input
+                                type="text"
+                                name="user-name"
+                                placeholder="Username"
+                              />
+                              <input
+                                type="password"
+                                name="user-password"
+                                placeholder="Password"
+                              />
+                              <div className="button-box">
+                                <div className="login-toggle-btn">
+                                  <input type="checkbox" />
+                                  <label className="ml-10">Remember me</label>
+                                  <Link to={process.env.PUBLIC_URL + "/"}>
+                                    Forgot Password?
+                                  </Link>
+                                </div>
+                                <button type="submit">
+                                  <span>Login</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="register">
+                        <div className="login-form-container">
+                          <div className="login-register-form">
+                            <form>
+                              <input
+                                type="text"
+                                name="username" value={userDetails.username} onChange={(e)=>changeHandle(e)}
+                                placeholder="Username"
+                              />
+                              <input
+                                type="password"
+                                name="password" value={userDetails.password} onChange={(e)=>changeHandle(e)}
+                                placeholder="Password"
+                              />
+                              <input
+                                name="email" value={userDetails.email} onChange={(e)=>changeHandle(e)}
+                                placeholder="Email"
+                                type="email"
+                              />
+                              <div className="button-box">
+                                <button type="submit" onClick={(e)=>{submit(e)}}>
+                                  <span>Register</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutOne>
+    </Fragment>
+  );
+};
+
+LoginRegister.propTypes = {
+  location: PropTypes.object
+};
+
+export default LoginRegister;
